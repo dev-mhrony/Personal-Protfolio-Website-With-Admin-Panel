4b188dbe36005b06b5848f2e813f646395a13f93
dev.mhrony <mh.rony.dhanvi@gmail.com>
2023-04-05 23:24:59 +0600

    project_start

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+
+//  =>  Template Name    : CCBD-Protfolio - Personal Portfolio HTML Template
+
+//  =>  Template Author  : Code Camp BD
+
+//  =>  Author Website   : https://developerrony.com/
+
+
+
+
 (function ($) {
     "use strict";
 
